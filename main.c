dd85207373d316ec776a27705e9987c7109d020e
Ryan Peters <c3320399@uon.edu.au>
2019-04-04 04:57:41 +0000

    Revert "This is mostly a skeleton and to be worked on further... some things were taken from github and im not entirely sure how they work "
    This reverts commit 9f842b4bae6d4568fe3077de70c5c1a0d0d416e7.

--- main.c
+++ main.c
@@ -1,42 +1,5 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <studio.h>
 
-
-void cipher(char* plainText, int key);
-
-int main(void){
-	
-	int key = 1;
-	char plainText[101];
-	
-	
-	printf("Please enter the text you want to encrypt: \n");
-	printf("You are encrypting:  ");
-
-	
-	fgets(plainText, sizeof(plainText), stdin);
-	
-	
-	printf("\nThe ciphered text is : ");
-	
-
-	cipher(plainText, key);
-
-}
-
-void cipher(char* plainText, int key){
-	
-	int i=0;
-	int cypherValue;
-	char cypher;
-	
-	while( plainText[i] != '\0'){//&& strlen(plainText)-1 > i{
-		cypherValue = ((int)plainText[i] -97 + key) + 97;
-		cypher = (char)(cypherValue);
-		
-		printf("%c", cypher);
-		i++;
-	}
-	printf("\n");
+int main(){ 
+  return 0;
 }

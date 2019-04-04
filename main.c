26bdfd236fd609df8103028c5347532385ab0a33
Ryan Peters <c3320399@uon.edu.au>
2019-04-04 04:59:05 +0000

    This is the first shot, some of the code is taken from github and im yet to understand how they work but the cipher code kind of works at this point

--- main.c
+++ main.c
@@ -1,5 +1,42 @@
-#include <studio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-int main(){ 
-  return 0;
+
+void cipher(char* plainText, int key);
+
+int main(void){
+	
+	int key = 1;
+	char plainText[101];
+	
+	
+	printf("Please enter the text you want to encrypt: \n");
+	printf("You are encrypting:  ");
+
+	
+	fgets(plainText, sizeof(plainText), stdin);
+	
+	
+	printf("\nThe ciphered text is : ");
+	
+
+	cipher(plainText, key);
+
+}
+
+void cipher(char* plainText, int key){
+	
+	int i=0;
+	int cypherValue;
+	char cypher;
+	
+	while( plainText[i] != '\0'){//&& strlen(plainText)-1 > i{
+		cypherValue = ((int)plainText[i] -97 + key) + 97;
+		cypher = (char)(cypherValue);
+		
+		printf("%c", cypher);
+		i++;
+	}
+	printf("\n");
 }

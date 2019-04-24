0bc828b3ca3b7ee0850b80d6614ea60b5ef64eed
Ryan Peters <c3320399@uon.edu.au>
2019-04-24 16:14:40 +0000

    Now with comments!

--- encrypt.c
+++ encrypt.c
@@ -7,38 +7,38 @@
  */
 int main(void){
 	
-int i, x;
-   char str[1000]; //this is the cipher text
-   int choice;//for the swtich 
+int i,
+   char message[1000]; //this is the cipher text
+   int selection;//for the swtich 
    int key; //how much the code gets rotated
  
  printf("\nPlease enter text to cipher: ");
-   gets(str); //Gets used as scanf doesnt recognise spaces
+   gets(message); //Gets used as scanf doesnt recognise spaces
    
        printf("Select an option\n");
    printf("encrypt - 1\n");//goes to switch case
    printf("decrypt - 2\n");
-   scanf("%d", &choice); //detects to decrypt or encrypt
-   switch(choice)
+   scanf("%d", &selection); //detects to decrypt or encrypt
+   switch(selection)
 {
       case 1:
       {
    
    printf("\nSelect key rotation:\n");
    scanf("%d", &key);
-    for(i = 0; (i < 100 && str[i] != '\0'); i++) //for the array of str in terms of numerals
-        str[i] = str[i] + key; //string as ascii rotated by numeral set by key 
+    for(i = 0; (i < 100 && message[i] != '\0'); i++) //for the array of message in terms of numerals
+        message[i] = message[i] + key; //messageing as ascii rotated by numeral set by key 
 
-      printf("\nEncrypted string: %s\n", str);
+      printf("\nEncrypted messageing: %s\n", message);
       break;
 }
       case 2: 
       {
    printf("\nSelect key rotation:\n");
    scanf("%d", &key);
-    for(i = 0; (i < 100 && str[i] != '\0'); i++)
-        str[i] = str[i] - key; //taking away the key from the already encrypted text to decrypt
-      printf("\nDecrypted string: %s\n", str);
+    for(i = 0; (i < 100 && message[i] != '\0'); i++)
+        message[i] = message[i] - key; //taking away the key from the already encrypted text to decrypt
+      printf("\nDecrypted messageing: %s\n", message);
       break;
 }
 
